659a9c0a4e6ef769814c76c9ce468d63bc4e8a38
ovidiuapostol <oapostol@gmail.com>
2026-02-28 11:13:17 +0100

    Initial C++ project with Makefile and Jenkinsfile

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    std::cout << "----------------------"<< std::endl;
+    std::cout<< "Greetings form your C++ Jenkins pipeline!"<< std::endl;
+    std::cout<<"------------------------"<< std::endl;
+    return 0;
+}

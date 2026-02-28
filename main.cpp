03e942235e391009b5757ba8ad7f20506b0aeb20
ovidiuapostol <oapostol@gmail.com>
2026-02-28 16:12:40 +0100

    version added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#define version 1
 int main()
 {
     std::cout << "----------------------"<< std::endl;
     std::cout<< "Greetings form your C++ Jenkins pipeline!"<< std::endl;
     std::cout<<"------------------------"<< std::endl;
+    std::cout<<"version: " << version<<std::endl;
     return 0;
 }
